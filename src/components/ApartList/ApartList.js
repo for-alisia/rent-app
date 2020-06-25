0faa1a2611c7b29ce04c2aa5b090a20a5535b82c
for-alisia <for-alisia@yandex.ru>
2020-06-25 12:17:28 +0500

    Finished Overview layout

--- src/components/ApartList/ApartList.js
+++ src/components/ApartList/ApartList.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { getAparts, getError, getIsLoading, fetchApartsRequest } from '../../store/aparts';
 
 import ApartListItem from '../ApartListItem';
 import Spinner from '../Spinner';
+import { ReactComponent as ArrowRightIcon } from '../../icons/Arrow.svg';
 
 import './ApartList.scss';
 
@@ -29,6 +31,12 @@ const ApartList = (props) => {
           </li>
         );
       })}
+      <li className="apart__view-all">
+        <Link to="/apartaments">
+          Посмотреть все варианты
+          <ArrowRightIcon />
+        </Link>
+      </li>
     </ul>
   );
 };
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,7 +2,22 @@ import React from 'react';
 import './Spinner.scss';
 
 const Spinner = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="spinner">
+      <div className="spinner__blocks">
+        <div className="spinner__main">
+          <div className="spinner__first"></div>
+          <div className="spinner__second"></div>
+          <div className="spinner__third"></div>
+          <div className="spinner__fourth"></div>
+          <div className="spinner__fifth"></div>
+          <div className="spinner__sixth"></div>
+          <div className="spinner__seventh"></div>
+          <div className="spinner__eight"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
